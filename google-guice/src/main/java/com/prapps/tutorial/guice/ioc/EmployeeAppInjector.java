b34f15e542ca65a1895ad644b5928a4ba145a309
pratik006 <pratik006@gmail.com>
2015-01-06 22:58:56 +0530

    added log interceptor

--- google-guice/src/main/java/com/prapps/tutorial/guice/ioc/EmployeeAppInjector.java
+++ google-guice/src/main/java/com/prapps/tutorial/guice/ioc/EmployeeAppInjector.java
@@ -1,6 +1,9 @@
 package com.prapps.tutorial.guice.ioc;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import com.google.inject.matcher.Matchers;
+import com.prapps.tutorial.guice.aop.LogAspect;
 import com.prapps.tutorial.guice.service.api.EmployeeService;
 import com.prapps.tutorial.guice.service.impl.EmployeeServiceImpl;
 
@@ -8,6 +11,9 @@ public class EmployeeAppInjector extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(EmployeeService.class).to(EmployeeServiceImpl.class);		
+		bind(EmployeeService.class).to(EmployeeServiceImpl.class).in(Singleton.class);
+		LogAspect logAspect = new LogAspect();
+		requestInjection(logAspect);
+		bindInterceptor(Matchers.subclassesOf(EmployeeService.class), Matchers.any(), logAspect);
 	}
 }
