458f222b9195798e021c1964687f5d2d9f9ba24a
pratik006 <pratik006@gmail.com>
2015-01-22 10:44:49 +0530

    removed commented lines

--- google-guice-jpa/src/main/java/com/prapps/tutorial/guice/service/impl/EmployeeServiceImpl.java
+++ google-guice-jpa/src/main/java/com/prapps/tutorial/guice/service/impl/EmployeeServiceImpl.java
@@ -12,12 +12,10 @@ import com.prapps.tutorial.guice.vo.Employee;
 
 public class EmployeeServiceImpl implements EmployeeService {
 
-	//private Map<Long, Employee> employeeMap = new HashMap<Long, Employee>();
 	@Inject private EmployeeDao employeeDao;
 	
 	@Transactional
 	public void addEmployee(Employee employee) {
-		/*employeeMap.put(employee.getId(), employee);*/
 		com.prapps.tutorial.guice.entities.Employee emp = new com.prapps.tutorial.guice.entities.Employee(
 				employee.getId(),
 				employee.getName(),
@@ -28,11 +26,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	public Collection<Employee> searchEmployeeByName(String name) {
 		Collection<Employee> result = new ArrayList<Employee>();
-		/*for(Entry<Long, Employee> entry : employeeMap.entrySet()) {
-			if(entry.getValue().getName().equals(name)) {
-				result.add(entry.getValue());
-			}
-		}*/
 		List<com.prapps.tutorial.guice.entities.Employee> employees = employeeDao.findEmployee(name);
 		for(com.prapps.tutorial.guice.entities.Employee emp : employees) {
 			result.add(new Employee(emp.getId(), emp.getName(), emp.getSalary()));
@@ -41,7 +34,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 	}
 
 	public Employee searchEmployeeById(Long id) {
-		//return employeeMap.get(id);
 		com.prapps.tutorial.guice.entities.Employee employee = employeeDao.findEmployeeById(id);
 		Employee empvo = new Employee();
 		empvo.setId(employee.getId());
