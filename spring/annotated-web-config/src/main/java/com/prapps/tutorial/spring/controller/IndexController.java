43ce40cce6053234907fad91ce248be55b447ce6
pratik006 <pratik006@gmail.com>
2015-02-19 22:39:19 +0530

    initial import

---
+++ spring/annotated-web-config/src/main/java/com/prapps/tutorial/spring/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.prapps.tutorial.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+}
