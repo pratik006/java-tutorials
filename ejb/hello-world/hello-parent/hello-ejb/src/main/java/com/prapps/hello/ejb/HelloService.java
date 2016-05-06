5f5d98bff01844321990ba95d69d531f3d237841
Pratik Sengupta <pratik006@gmail.com>
2016-05-06 05:53:29 -0700

    not working, inital pom changes

--- ejb/hello-world/hello-parent/hello-ejb/src/main/java/com/prapps/hello/ejb/HelloService.java
+++ ejb/hello-world/hello-parent/hello-ejb/src/main/java/com/prapps/hello/ejb/HelloService.java
@@ -7,6 +7,8 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+import com.prapps.generated.Hello;
+
 /**
  * Session Bean implementation class HelloWorldBean
  */
@@ -15,7 +17,7 @@ import javax.jws.soap.SOAPBinding;
 @WebService(name="Hello", serviceName="HelloService")
 @SOAPBinding(parameterStyle=SOAPBinding.ParameterStyle.BARE)
 @Interceptors(TimerLog.class)
-public class HelloService implements HelloWorldBeanRemote {
+public class HelloService implements HelloWorldBeanRemote, Hello {
 
     /**
      * Default constructor. 
@@ -25,18 +27,27 @@ public class HelloService implements HelloWorldBeanRemote {
     }
 
 	@Override
-	@WebMethod
 	public String sayHelloRemote() {
 		System.out.println("sayHelloRemote..");
 		return "Hello World !!!";
 	}
 
 	@Override
-	@WebMethod
 	public HelloResponse sayHelloRemoteDetail(HelloRequest helloRequest) {
 		HelloResponse helloResponse = new HelloResponse();
 		helloResponse.setId(helloRequest.getId()+100);
 		helloResponse.setResp("hello "+helloRequest.getKey());
 		return helloResponse;
+	}
+	
+	@Override
+	@WebMethod
+	public String sayHello() {
+		return sayHelloRemote();
+	}
+
+	@Override
+	public com.prapps.generated.HelloResponse sayHelloDetail(com.prapps.generated.HelloRequest sayHelloDetail) {
+		return sayHelloDetail(sayHelloDetail);
 	}
 }
