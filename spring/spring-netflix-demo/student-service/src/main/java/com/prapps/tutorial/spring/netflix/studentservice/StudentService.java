45661cb7fdc0e3d5bc61d446550559e5c2a743dc
Pratik Sengupta <pratik006@gmail.com>
2019-04-05 23:45:30 +0530

    added hystrix example | working

--- spring/spring-netflix-demo/student-service/src/main/java/com/prapps/tutorial/spring/netflix/studentservice/StudentService.java
+++ spring/spring-netflix-demo/student-service/src/main/java/com/prapps/tutorial/spring/netflix/studentservice/StudentService.java
@@ -1,19 +1,19 @@
 package com.prapps.tutorial.spring.netflix.studentservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping
 public class StudentService {
 
-    //@LoadBalanced
     private StudentConsumerService studentConsumerService;
 
     @Autowired
@@ -25,5 +25,20 @@ public class StudentService {
     public List<Student> findStudentsByName(@RequestParam ("name") String name) {
         System.out.println("hashCode of studentConsumerService: "+studentConsumerService.hashCode());
         return studentConsumerService.findStudentsByName(name);
+    }
+
+
+    @GetMapping("/tryluck")
+    @HystrixCommand(fallbackMethod = "tryLuckFallback", commandKey = "failluck", groupKey = "failluck")
+    public String tryLuck() {
+        if (new Random().nextBoolean()) {
+            throw new RuntimeException();
+        }
+
+        return "success";
+    }
+
+    public String tryLuckFallback() {
+        return "fallback for tryluck initiated";
     }
 }
--- spring/spring-netflix-demo/student-service/src/main/java/com/prapps/tutorial/spring/netflix/studentservice/StudentServiceApplication.java
+++ spring/spring-netflix-demo/student-service/src/main/java/com/prapps/tutorial/spring/netflix/studentservice/StudentServiceApplication.java
@@ -2,10 +2,14 @@ package com.prapps.tutorial.spring.netflix.studentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class StudentServiceApplication {
 
 	public static void main(String[] args) {
