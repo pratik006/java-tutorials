ac0558589591345aa41bd30e70f34e3b03adab22
Pratik Sengupta <pratik006@gmail.com>
2019-04-11 23:39:59 +0530

    working with logs | spring sleuth

--- spring/spring-netflix-demo/eureka-feign-client/src/main/java/com/prapps/tutorial/spring/netflix/client/consumer/GreetingClient.java
+++
@@ -1,8 +0,0 @@
-package com.prapps.tutorial.spring.netflix.client.consumer;
-
-import com.prapps.tutorial.spring.netflix.client.GreetingController;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient("spring-cloud-eureka-client")
-public interface GreetingClient extends GreetingController {
-}
