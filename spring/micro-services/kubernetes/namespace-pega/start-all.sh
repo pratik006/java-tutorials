1b8602abf0fc3d765cf4002e8dcadb8b29d7ff4e
Pratik Sengupta <pratik006@gmail.com>
2019-04-26 02:06:23 +0530

    namespace needs full FQDN in VirtualService

--- spring/micro-services/kubernetes/namespace-pega/start-all.sh
+++ spring/micro-services/kubernetes/namespace-pega/start-all.sh
@@ -14,4 +14,10 @@ export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 export INGRESS_HOST=$(minikube ip)
 
-# curl -I -HHost:pegasample.com http://$INGRESS_HOST:$INGRESS_PORT/search/
+# curl -I -HHost:pegasample.com http://$INGRESS_HOST:$INGRESS_PORT/search?name=Pratik
+
+# to port forward the gateway to localhost
+# sudo kubectl -n istio-system port-forward istio-ingressgateway-d67598f4-s54lv 80
+
+# Envoy Admin console
+# kubectl -n istio-system port-forward istio-ingressgateway-d67598f4-s54lv 15000
