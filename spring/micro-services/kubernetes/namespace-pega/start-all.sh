3f3d080b07ca5d7c0f3b167a8e0b7ccc7f602c24
Pratik Sengupta <pratik006@gmail.com>
2019-04-29 00:35:49 +0530

    added portforwarding

--- spring/micro-services/kubernetes/namespace-pega/start-all.sh
+++ spring/micro-services/kubernetes/namespace-pega/start-all.sh
@@ -50,3 +50,5 @@ kubectl apply -f external-gatewal.yaml
 # port forward Grafana dashboard
 # kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}') 3000:3000 &
 # http://localhost:3000/dashboard/db/istio-mesh-dashboard
+# Prometheus port forwarding
+# kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090 &
